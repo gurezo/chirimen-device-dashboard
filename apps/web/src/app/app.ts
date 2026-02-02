7d7ae1267d20a6b0b8eb1779df9dcfbe91ccc648
akihiko.kigure <akihiko.kigure@gmail.com>
2026-02-02 23:16:43 +0900

    feat(web): add Angular frontend with Material and Tailwind
    - Add @nx/angular plugin
    - Generate Angular app at apps/web with Tailwind CSS
    - Add Angular Material with violet/rose theme
    - Add Material + Tailwind demo to app component
    - Add Nx scripts: start, build, test, lint, e2e
    
    Closes #2

---
+++ apps/web/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { NxWelcome } from "./nx-welcome";
+
+@Component({
+  imports: [NxWelcome, RouterModule, MatButtonModule],
+  selector: "app-root",
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
+})
+export class App {
+  protected title = "web";
+}
